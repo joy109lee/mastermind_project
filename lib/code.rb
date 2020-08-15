753ab31eb1e6f41543ffacfb254567064073faf3
Joy Lee <joy109lee@gmail.com>
2020-08-15 01:49:05 -0700

    self.valid

---
+++ lib/code.rb
@@ -0,0 +1,17 @@
+class Code
+  POSSIBLE_PEGS = {
+    "R" => :red,
+    "G" => :green,
+    "B" => :blue,
+    "Y" => :yellow
+  }
+
+  def self.valid_pegs?(arr)
+    arr.all? {|char| self.POSSIBLE_PEGS.include?(char)}
+      return true
+    else
+      false
+    end
+  end
+
+end
