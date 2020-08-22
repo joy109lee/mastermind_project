56e63d25620d1cdfe644f344a0ce2f497da22dca
Joy Lee <joy109lee@gmail.com>
2020-08-21 20:00:58 -0700

    MAstermind initialize random_code

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,11 +1,15 @@
 require_relative "code"
 
 class Mastermind
+
     def initialize(length)
         @secret_code = []
+        random_code = Code.new(["R", "G", "R", "B"])
         length.times do
-            @secret_code << POSSIBLE_PEGS.sample
+            @secret_code << random_code.pegs.sample
         end
     end
+
+    def
 
 end
