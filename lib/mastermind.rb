fe0f59612b3d13d1ce9612060d4ebff725d7ce81
Joy Lee <joy109lee@gmail.com>
2020-08-21 20:17:24 -0700

    print_matches finished run time error

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -7,8 +7,8 @@ class Mastermind
     end
 
     def print_matches(code)
-        puts Code.num_exact_matches(code)
-        puts Code.num_near_matches(code)
+        puts code.num_exact_matches(@secret_code)
+        puts code.num_near_matches(@secret_code)
     end
 
 end
