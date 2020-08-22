90aecd4be9fdf254869e6249e8dcb37332788ea3
Joy Lee <joy109lee@gmail.com>
2020-08-21 20:40:09 -0700

    ask for user

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -11,4 +11,12 @@ class Mastermind
         puts code.num_near_matches(@secret_code)
     end
 
+    def ask_user_for_guess
+        puts "Enter a code"
+        user_code = gets.chomp
+        Code.from_string(user_code)
+        
+
+    end
+
 end
