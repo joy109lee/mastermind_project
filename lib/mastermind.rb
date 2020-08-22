bee13fde20c6f02c5300a9705956eb476adf4d63
Joy Lee <joy109lee@gmail.com>
2020-08-21 19:43:10 -0700

    MAstermind initialize

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,5 +1,11 @@
 require_relative "code"
 
 class Mastermind
+    def initialize(length)
+        @secret_code = []
+        length.times do
+            @secret_code << POSSIBLE_PEGS.sample
+        end
+    end
 
 end
